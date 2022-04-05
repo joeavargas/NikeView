8371ee0ac5413a90e754d5f08bfa393270f93206
Joe Vargas <joeavargas@outlook.com>
2022-04-05 14:42:31 -0500

    Embedded the entire view in vertical scrollview.

--- NikeView/Screens/HomeView.swift
+++ NikeView/Screens/HomeView.swift
@@ -12,77 +12,81 @@ struct HomeView: View {
     var offsetY: CGFloat = 100
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Discover")
-                        .font(.system(size: 32))
-                        .fontWeight(.medium)
-                    Text(hvm.todaysDate())
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                }//: VStack
-                Spacer()
-            }//: HStack
-            .padding()
-            
-            // MARK: - Headline card
-            ZStack {
-                Image("1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
+        ScrollView(.vertical) {
+            VStack {
                 HStack {
                     VStack(alignment: .leading) {
-                        Text("Just In")
-                            .font(.subheadline)
-                        Text("Shop Mens and Women's apparal")
-                            .font(.largeTitle)
-                            .bold()
-                    }
-                    .foregroundColor(.white)
-                    .offset(y: offsetY)
+                        Text("Discover")
+                            .font(.system(size: 32))
+                            .fontWeight(.medium)
+                        Text(hvm.todaysDate())
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }//: VStack
                     Spacer()
                 }//: HStack
                 .padding()
                 
-            }//: ZStack
-            
-            // MARK: - What's new section
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("What's new")
-                        .fontWeight(.medium)
-                    Text("The latest arrivals from Nike")
-                        .font(.title)
-                        .foregroundColor(.secondary)
-                }
-                Spacer()
-            }//: VStack
-            .padding()
-            
-            // MARK: - Latest arrivals from Nike / Horizontal scroll
-            ScrollView(.horizontal, showsIndicators: false) {
+                // MARK: - Headline card
+                ZStack {
+                    Image("1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Just In")
+                                .font(.subheadline)
+                            Text("Shop Mens and Women's apparal")
+                                .font(.largeTitle)
+                                .bold()
+                        }
+                        .foregroundColor(.white)
+                        .offset(y: offsetY)
+                        Spacer()
+                    }//: HStack
+                    .padding()
+                    
+                }//: ZStack
+                
+                // MARK: - What's new section
                 HStack {
-                    ForEach(1..<10) { _ in
-                        VStack {
-                            Image("Air Jordan XXXVI")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("Air Jordan XXXVI")
-                                        .padding(.bottom, 3)
-                                    Text("$150")
+                    VStack(alignment: .leading) {
+                        Text("What's new")
+                            .fontWeight(.medium)
+                        Text("The latest arrivals from Nike")
+                            .font(.title)
+                            .foregroundColor(.secondary)
+                    }
+                    Spacer()
+                }//: VStack
+                .padding()
+                
+                // MARK: - Latest arrivals from Nike / Horizontal scroll
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(1..<10) { _ in
+                            VStack {
+                                Image("Air Jordan XXXVI")
+                                    .resizable()
+                                    .frame(width: 300, height: 200)
+                                    .aspectRatio(contentMode: .fill)
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text("Air Jordan XXXVI")
+                                            .padding(.bottom, 3)
+                                        Text("$150")
+                                    }
+                                    Spacer()
                                 }
-                                Spacer()
+                                .padding()
                             }
-                            .padding()
                         }
                     }
                 }
+                
+                // MARK: - Feature article cards
             }
             
-            // MARK: - Feature article cards
         }
     }
 }
