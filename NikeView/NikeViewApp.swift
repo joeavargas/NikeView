eec44610cd5078bf92faa62673dd49385a8a38cc
Joe Vargas <joeavargas@outlook.com>
2022-04-04 21:44:20 -0500

    commit skeleton project

---
+++ NikeView/NikeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeViewApp.swift
+//  NikeView
+//
+//  Created by Joe Vargas on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NikeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
