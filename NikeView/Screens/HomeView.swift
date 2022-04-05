63e0beedc9c1c163c1d8c97847288629434ad9f4
Joe Vargas <joeavargas@outlook.com>
2022-04-04 21:58:36 -0500

    HomeView configured to MainView tabview

---
+++ NikeView/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NikeView
+//
+//  Created by Joe Vargas on 4/4/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
