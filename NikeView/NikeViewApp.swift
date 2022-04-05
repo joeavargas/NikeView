543f157b471ed53769b40fbacaf404f878fc29b8
Joe Vargas <joeavargas@outlook.com>
2022-04-04 21:50:38 -0500

    MainView created and configured to NikeViewApp

--- NikeView/NikeViewApp.swift
+++ NikeView/NikeViewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NikeViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ NikeView/Screens/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  NikeView
+//
+//  Created by Joe Vargas on 4/4/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("MainView")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
